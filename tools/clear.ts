cf75d988bb51252473afeb888b8efe7325e6557c
imeepos <1037483576@qq.com>
2019-05-30 00:35:24 +0800

    feat(dev) add tools

--- tools/clear.ts
+++ tools/clear.ts
@@ -1,15 +1,7 @@
-import { exec } from 'shelljs';
-async function clear(){
-    await execSync(`find ./demos/**/*.js -exec rm -rf {} \;`)
-    await execSync(`find ./demos/**/*.d.ts -exec rm -rf {} \;`)
-    await execSync(`find ./libs/**/*.js -exec rm -rf {} \;`)
-    await execSync(`find ./libs/**/*.d.ts -exec rm -rf {} \;`)
-}
-export function execSync(command: string){
-    return new Promise((resolve,reject)=>{
-        exec(command,()=>{
-            resolve();
-        })
+import rimraf from 'rimraf';
+async function clear() {
+    rimraf(`{demos,libs}/**/*.{js,d.ts}`,()=>{
+        console.log(`rimraf`)
     })
 }
 clear();
