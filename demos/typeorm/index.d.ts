a8d4c8b68195ddb143e1ed8b6001d6a44e28b7a5
imeepos <1037483576@qq.com>
2019-05-29 16:10:27 +0800

    merge dev to master

--- demos/typeorm/index.d.ts
+++ demos/typeorm/index.d.ts
@@ -13,5 +13,5 @@ import { MemberEntity } from './entities/member.entity';
 import { PostEntity } from './entities/post.entity';
 import { PostCategoryEntity } from './entities/postCategory.entity';
 export { AccessTokenEntity, AddonEntity, LoggerEntity, OrganizationEntity, PermissionEntity, RefreshTokenEntity, RoleEntity, UserEntity, ArticleEntity, ArticleCategoryEntity, ArticleCommentEntity, EXPRES_TIME };
-declare const _default: (typeof RoleEntity | typeof PermissionEntity | typeof AccessTokenEntity | typeof AddonEntity | typeof LoggerEntity | typeof OrganizationEntity | typeof RefreshTokenEntity | typeof UserEntity | typeof ArticleEntity | typeof ArticleCategoryEntity | typeof ArticleCommentEntity | typeof MemberEntity | typeof PostEntity | typeof PostCategoryEntity)[];
+declare const _default: (typeof AddonEntity | typeof LoggerEntity | typeof OrganizationEntity | typeof PermissionEntity | typeof RefreshTokenEntity | typeof RoleEntity | typeof UserEntity | typeof AccessTokenEntity | typeof ArticleEntity | typeof ArticleCategoryEntity | typeof ArticleCommentEntity | typeof MemberEntity | typeof PostEntity | typeof PostCategoryEntity)[];
 export default _default;
